309cf8302e429593155f1bd5e15c679815f16290
Ogabek Erkinov <ogabekerkinov56@gmail.com>
2025-05-11 09:31:50 +0500

    telegram-bot

---
+++ keyboards.js
@@ -0,0 +1,19 @@
+const getDefaultKeyboard = () => ({
+  reply_markup: {
+    keyboard: [[{ text: "🔄 Restart" }]],
+    resize_keyboard: true,
+    one_time_keyboard: false
+  }
+});
+
+
+const getConfirmKeyboard = () => ({
+  reply_markup: {
+    inline_keyboard: [
+      [{ text: "✅ Tasdiqlash", callback_data: "confirm" }],
+      [{ text: "✏️ Tahrirlash", callback_data: "edit" }]
+    ]
+  }
+});
+
+module.exports = { getDefaultKeyboard, getConfirmKeyboard };
